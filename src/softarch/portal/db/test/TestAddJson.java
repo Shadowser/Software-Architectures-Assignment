37e43b54efb9612dbb4cc1b56ac5837cc85f556a
Ken Vernaillen <Ken Vernaillen>
2016-03-24 19:00:27 +0100

    relative path json

--- src/softarch/portal/db/test/TestAddJson.java
+++ src/softarch/portal/db/test/TestAddJson.java
@@ -13,7 +13,7 @@ import softarch.portal.db.json.JsonDB;
 public class TestAddJson {
 	public static void main(String[] args) {
 		try {
-			JsonDB jsonDB = new JsonDB("C:\\Users\\Ken Vernaillen\\OneDrive\\School 2015-2016\\Software Architectures\\Assignments\\assignment 2\\New Workspace\\jsondatabase.json");
+			JsonDB jsonDB = new JsonDB("jsondatabase.json");
 			CheapSubscription test = new CheapSubscription("UsernameTest", "PasswordTest", "FirstNameTest", "LastNameTest", "EmailTest", new Date());
 			jsonDB.insertUserProfile(test);
 		}
