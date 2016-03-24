3ae632c64e4b19b23af70af4bf908e6f30c07a56
Kevin Van Ryckegem <kevin.van.ryckegem@ehb.be>
2016-03-24 23:04:27 +0100

    Comment

--- src/softarch/portal/db/DatabaseContract.java
+++ src/softarch/portal/db/DatabaseContract.java
@@ -13,14 +13,9 @@ public class DatabaseContract {
 	    public static final String COL_TYPE = "Type";
 	}
 	
+	// Similar to the Table_Users should be done for the Table_Regular
 	public static abstract class Table_Regular {
 	    public static final String TABLE_NAME = "regulardata";
-	    public static final String COL_USERNAME = "Username";
-	    public static final String COL_PASSWORD = "Password";
-	    public static final String COL_FIRST_NAME = "FirstName";
-	    public static final String COL_LAST_NAME = "LastName";
-	    public static final String COL_EMAIL = "EmailAddress";
-	    public static final String COL_LAST_LOGIN = "LastLogin";
 	    public static final String COL_TYPE = "Type";
 	}
 	
